f9dc0a4d1b48a2a19316af4921bc071bf7517e40
EpicET <etowner@middlebury.edu>
2026-03-30 23:28:17 -0400

    Modified main test

--- backend/src/test/java/com/app/bank/BankApplicationTests.java
+++ backend/src/test/java/com/app/bank/BankApplicationTests.java
@@ -2,8 +2,12 @@ package com.app.bank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.UseMainMethod;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+@WebAppConfiguration
 class BankApplicationTests {
 
 	@Test
