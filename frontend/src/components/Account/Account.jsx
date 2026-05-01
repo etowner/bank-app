74517753279bf23c5dfa9bd1c3adba676f92fdc8
EpicET <etowner@middlebury.edu>
2026-05-01 18:17:20 -0400

    everything seems to work

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -11,30 +11,27 @@ import CloseAccount from "./CloseAccount";
 import LineChart from "./LineChart"
 import { UserContext } from "../../UserContext";
 
+function CustomToggle({ children, eventKey }) {
+    const showAction = useAccordionButton(eventKey, () => {});
+    
+    return (
+      <Button variant="dark" onClick={showAction} className="mb-3">
+        {" "}
+        {children}
+      </Button>
+    );
+  }
+
 const Account = () => {
   const { userID } = useContext(UserContext) 
   const { accountID } = useParams();
-  const [account, setAccount] = useState([]);
-  const [type, setType] = useState("");
-  const [balance, setBalance] = useState();
-  const [transHistory, setTransHistory] = useState([]);
+  const [account, setAccount] = useState(null);
 
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(`/home`);
   };
-
-  function CustomToggle({ children, eventKey }) {
-    const decoratedOnClick = useAccordionButton(eventKey, onclick);
-
-    return (
-      <Button variant="dark" onClick={decoratedOnClick} className="mb-3">
-        {" "}
-        {children}
-      </Button>
-    );
-  }
 
   const updateAccount = (updatedAccount) => {
     setAccount(updatedAccount);
@@ -52,22 +49,6 @@ const Account = () => {
       });
   }, [accountID, userID]);
 
-  // Update account state when account is fetched && when transaction is made
-  useEffect(() => {
-    if (account) {
-      Object.entries(account).forEach(([key, value]) => {
-        if (key === "userID") {
-        } else if (key === "type") {
-          setType(value);
-        } else if (key === "balance") {
-          setBalance(value);
-        } else if (key === "transHistory") {
-          setTransHistory(value);
-        }
-      });
-    }
-  }, [account, transHistory]);
-
   return (
     <div className="Home">
       <Navbar bg="dark" variant="dark" className="mb-5">
@@ -84,11 +65,11 @@ const Account = () => {
         {/* Account Info */}
         <Row className="mb-3">
           <h1>
-            {type} {accountID}
+            {account?.type} {accountID}
           </h1>
         </Row>
         <Row className="mb-3">
-          <h3>Balance: ${balance} </h3>
+          <h3>Balance: ${account?.balance} </h3>
         </Row>
 
         <Row className="mb-4 justify-content-md-center">
@@ -104,17 +85,19 @@ const Account = () => {
                   <thead>
                     <tr>
                       <th>#</th>
-                      <th>type</th>
-                      <th>amount</th>
+                      <th>Type</th>
+                      <th>Amount</th>
+                      <th>Date</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {transHistory &&
-                      Object.entries(transHistory).map(([key, value]) => (
+                    {account?.transHistory &&
+                      Object.entries(account.transHistory).map(([key, value]) => (
                         <tr key={key}>
                           <td>{key}</td>
                           <td>{value.type}</td>
                           <td>{value.amount}</td>
+                          <td>{new Date(value.timestamp).toLocaleString()}</td>
                         </tr>
                       ))}
                   </tbody>
@@ -125,9 +108,7 @@ const Account = () => {
           {/* Account Actions */}
           <Col sm={5}>
             <Card border="dark" style={{ width: "30rem" }}>
-              <Card.Header
-                style={{ color: "white", backgroundColor: "#282c34" }}
-              >
+              <Card.Header style={{ color: "white", backgroundColor: "#282c34" }}>
                 <h3>Transaction Options</h3>
               </Card.Header>
               <Card.Body>
@@ -136,11 +117,7 @@ const Account = () => {
                   <Col>
                     <Accordion.Collapse eventKey="0">
                       <Card.Body>
-                        <Deposit
-                          userID={userID}
-                          accountID={accountID}
-                          updateAccount={updateAccount}
-                        />
+                        <Deposit updateAccount={updateAccount}/> 
                       </Card.Body>
                     </Accordion.Collapse>
                   </Col>
@@ -148,12 +125,7 @@ const Account = () => {
                     <CustomToggle eventKey="1">Withdraw</CustomToggle>
                     <Accordion.Collapse eventKey="1">
                       <Card.Body>
-                        <Withdraw
-                          userID={userID}
-                          accountID={accountID}
-                          balance={balance}
-                          updateAccount={updateAccount}
-                        />
+                        <Withdraw balance={account?.balance} updateAccount={updateAccount}/>
                       </Card.Body>
                     </Accordion.Collapse>
                   </Col>
@@ -164,7 +136,7 @@ const Account = () => {
         </Row>
         <Row className="mb-3">
           <Card>
-            <LineChart accountID={accountID} transHistory={transHistory} />
+            <LineChart accountID={accountID} transHistory={account?.transHistory} />
           </Card>
         </Row>
         <Row className="md-5 justify-content-md-center">
